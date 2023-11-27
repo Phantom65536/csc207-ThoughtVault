64f099d192d119275741f906052a339c37e3136d
Kyssen Yu <80001648+Kyssen@users.noreply.github.com>
2023-11-26 20:05:43 -0500

    updated EventInputData to have correct attributes to work with Note

--- src/main/java/InputData/EventInputData.java
+++ src/main/java/InputData/EventInputData.java
@@ -1,22 +1,34 @@
 package InputData;
 
+import entity.LabelCat;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class EventInputData {
-    private int userid;
-    private String username;
-    private String password;
-    private String credential;
-
+    private final int ID;
+    private String title;
+    private final int userID;
+    private String location;
+    private String description;
+    private LabelCat label;
+    private boolean pinned;
+    private ArrayList<Integer> descendantsID;
     private LocalDate date;
     private LocalTime startTime = LocalTime.MIDNIGHT;                 // default starttime when user specifies whole-day
     private LocalTime endTime = LocalTime.of(23, 59);
-    public EventInputData (int userid, String username, String password, String credential, LocalDate date, LocalTime startTime, LocalTime endTime) {
-        this.userid = userid;
-        this.username = username;
-        this.password = password;
-        this.credential = credential;
+    public EventInputData (int ID, String title, int userID, String location,
+                           String description, LabelCat label, boolean pinned,
+                           ArrayList<Integer> descendantsID, LocalDate date,
+                           LocalTime startTime, LocalTime endTime) {
+        this.ID = ID;
+        this.title = title;
+        this.userID = userID;
+        this.location = location;
+        this.description = description;
+        this.label = label;
+        this.pinned = pinned;
         this.date = date;
         this.startTime = startTime;
         this.endTime = endTime;
