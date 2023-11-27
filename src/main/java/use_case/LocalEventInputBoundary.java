7a7eb6d3733bdd9a88bbb7653803a1435afca1ea
Kyssen Yu <80001648+Kyssen@users.noreply.github.com>
2023-11-26 19:51:26 -0500

    adjusted parameters of LocalEventInputBoundary to be EventInputData and eventID and user ID. Also added getAllEvents

--- src/main/java/use_case/LocalEventInputBoundary.java
+++ src/main/java/use_case/LocalEventInputBoundary.java
@@ -1,10 +1,16 @@
 package use_case;
 
 
+import InputData.EventInputData;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public interface LocalEventInputBoundary {
-    public void createEvent();
+    public void createEvent(EventInputData eventInputData);
     //create and save an event (make sure no sub-events are the same)
-    public void editEvent(); //edit an existing event and save its changes (make sure no sub-events are the same)
-    public void deleteEvent(); //delete an existing event
-    public void getEvent(); //check whether the event belongs to the user and return its corresponding Event instance when the user wants to view its details
+    public void editEvent(EventInputData eventInputData); //edit an existing event and save its changes (make sure no sub-events are the same)
+    public void deleteEvent(EventInputData eventInputData); //delete an existing event
+    public EventOutputData getEvent(String eventID, String userID); //check whether the event belongs to the user and return its corresponding Event instance when the user wants to view its details
+    public ArrayList<EventOutputData> getAllEvents(String userID)
 }
