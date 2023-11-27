9290ea042f51131e78966679eaabb97d340b3b51
Kyssen Yu <80001648+Kyssen@users.noreply.github.com>
2023-11-26 19:54:13 -0500

    added EventOutputData and fixed LocalEventInputBoundary by importing EventOutputData and requiring getEvent and getAllEvents to return correct output

--- src/main/java/use_case/LocalEventInputBoundary.java
+++ src/main/java/use_case/LocalEventInputBoundary.java
@@ -2,6 +2,7 @@ package use_case;
 
 
 import InputData.EventInputData;
+import OutputData.EventOutputData;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -12,5 +13,5 @@ public interface LocalEventInputBoundary {
     public void editEvent(EventInputData eventInputData); //edit an existing event and save its changes (make sure no sub-events are the same)
     public void deleteEvent(EventInputData eventInputData); //delete an existing event
     public EventOutputData getEvent(String eventID, String userID); //check whether the event belongs to the user and return its corresponding Event instance when the user wants to view its details
-    public ArrayList<EventOutputData> getAllEvents(String userID)
+    public ArrayList<EventOutputData> getAllEvents(String userID);
 }
