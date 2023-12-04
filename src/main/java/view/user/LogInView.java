ec1c64a2ef6f364b357cc6fe9dd72cb23537d9c4
Elsie Chan <phantomsmithhk@gmail.com>
2023-12-04 02:52:45 -0500

    Added listeners and property change handling to LogInView

--- src/main/java/view/user/LogInView.java
+++ src/main/java/view/user/LogInView.java
@@ -1,6 +1,7 @@
 package view.user;
 
 import interface_adapter.log_in_out.LogInOutController;
+import interface_adapter.log_in_out.LogInState;
 import interface_adapter.log_in_out.LogInViewModel;
 import view.LabelTextPanel;
 
@@ -8,17 +9,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 public class LogInView extends JPanel implements ActionListener, PropertyChangeListener {
     private final LogInViewModel loginViewModel;
 
     final JTextField usernameInputField = new JTextField(15);
-    private final JLabel usernameErrorField = new JLabel();
-
     final JPasswordField passwordInputField = new JPasswordField(15);
-    private final JLabel passwordErrorField = new JLabel();
 
     final JButton logIn;
     final JButton switchToSignupView;
@@ -42,15 +44,101 @@ public class LogInView extends JPanel implements ActionListener, PropertyChangeL
         buttons.add(logIn);
         switchToSignupView = new JButton(LogInViewModel.SWITCH_VIEW_LABEL);
         buttons.add(switchToSignupView);
+
+        logIn.addActionListener(                // This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(logIn)) {
+                            LogInState currentState = loginViewModel.getState();
+                            if (currentState.getUsername() == null || currentState.getPassword() == null) {
+                                JOptionPane.showMessageDialog(null, "Please fill in all fields.");
+                            } else {
+                                try {
+                                    loginoutController.logIn(
+                                            currentState.getUsername(),
+                                            currentState.getPassword()
+                                    );
+                                } catch (GeneralSecurityException | IOException e) {
+                                    JOptionPane.showMessageDialog(null, "GcalDAO setUserCalendar throws " + e.getMessage());
+                                    System.out.println(e.getMessage());
+                                }
+                            }
+                        }
+                    }
+                }
+        );
+
+        switchToSignupView.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(switchToSignupView)) {
+                            loginoutController.switchToSignupView();
+                        }
+                    }
+                }
+        );
+
+        usernameInputField.addKeyListener(
+            new KeyListener() {
+                @Override
+                public void keyTyped(KeyEvent e) {
+                    LogInState currentState = loginViewModel.getState();
+                    String text = usernameInputField.getText() + e.getKeyChar();
+                    currentState.setUsername(text);
+                    loginViewModel.setState(currentState);
+                }
+
+                @Override
+                public void keyPressed(KeyEvent e) {
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+                }
+            }
+        );
+
+        passwordInputField.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        LogInState currentState = loginViewModel.getState();
+                        currentState.setPassword(String.valueOf(passwordInputField.getPassword()) + e.getKeyChar());
+                        loginViewModel.setState(currentState);
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+
+                    }
+                }
+        );
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(title);
+        this.add(usernameInfo);
+        this.add(passwordInfo);
+        this.add(buttons);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void actionPerformed(ActionEvent evt) {
+        System.out.println("Click " + evt.getActionCommand());
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
+        LogInState logInState = (LogInState) evt.getNewValue();
+        if (logInState.getUsernameError() != null) {
+            JOptionPane.showMessageDialog(null, logInState.getUsernameError());
+        } else if (logInState.getPasswordError() != null) {
+            JOptionPane.showMessageDialog(null, logInState.getPasswordError());
+        }
     }
 }
